60618b5aad262cf4863e4615530512644ec73beb
Sang172 <sang.ahn.94@gmail.com>
2025-02-12 01:06:40 -0800

    debug=true

--- app.py
+++ app.py
@@ -382,4 +382,4 @@ def forecast():
     return jsonify(forecast_data)
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
